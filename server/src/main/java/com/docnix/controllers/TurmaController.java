fdcc55f862dd376b9f780149b793d5d41abab1be
matheus_toledo <matheus20wat@gmail.com>
2021-05-04 12:15:57 -0300

    adição do controller de turma

---
+++ server/src/main/java/com/docnix/controllers/TurmaController.java
@@ -0,0 +1,48 @@
+package com.docnix.controllers;
+
+import com.docnix.entity.Turma;
+import com.docnix.service.TurmaService;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/turmas")
+public class TurmaController {
+    private static final TurmaService turmaService = new TurmaService();
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getTurmas() {
+        return Response.ok().entity(turmaService.listar()).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getTurma(@PathParam("id") Long id) {
+        return Response.ok().entity(turmaService.obter(id)).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response inserirTurma(Turma turma) {
+        return Response.status(Response.Status.CREATED).entity(turmaService.inserir(turma)).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response alteraTurma(Turma turma) {
+        return Response.ok().entity(turmaService.editar(turma)).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deletaTurma(@PathParam("id") Long id) {
+        turmaService.deletar(id);
+        return Response.ok().build();
+    }
+}
