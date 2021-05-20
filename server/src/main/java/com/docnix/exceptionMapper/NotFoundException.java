ce35e5c98bf6a6c0593b000a6f8c7424b448c2b9
matheus_toledo <matheus20wat@gmail.com>
2021-05-20 15:15:49 -0300

    Implementação do NotFoundException mapper

---
+++ server/src/main/java/com/docnix/exceptionMapper/NotFoundException.java
@@ -0,0 +1,20 @@
+package com.docnix.exceptionMapper;
+
+import com.docnix.errorHandler.ErrorObject;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+public class NotFoundException extends Exception implements ExceptionMapper<NotFoundException> {
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    @Override
+    public Response toResponse(NotFoundException exception) {
+        return Response.status(Response.Status.NOT_FOUND).entity(new ErrorObject(exception.getMessage())).build();
+    }
+}
